a54b14a5816a25a6983a2bc65932e1cf9eab7861
chillbuilds <will@chillbuilds.com>
2024-09-08 15:21:12 -0600

    added back button, and oled libs

--- lil-devil/lil-devil.ino
+++ lil-devil/lil-devil.ino
@@ -3,97 +3,163 @@
 // - park
 // - 
 
-const int leftBtn = 2;
-const int rightBtn = 3;
-const int centerBtn = 4;
-const int systemDelay = 500;
+const int leftBtn = 4;
+const int rightBtn = 5;
+const int selectBtn = 6;
+const int backBtn = 7;
+const int systemDelay = 200;
 
-unsigned long previousHungerMillis = 0;
-unsigned long previousPooMillis = 0;
-// const unsigned long interval = 6 * 3600UL * 1000UL; // 6 hours in milliseconds
-unsigned long hungerInterval = 4000UL;
-unsigned long pooInterval = 2000UL;
+unsigned long previoushungerLvlMillis = 0;
+unsigned long previouspooLvlMillis = 0;
+unsigned long hungerLvlInterval = 4000UL;
+unsigned long pooLvlInterval = 2000UL;
 unsigned long hapinessInterval = 3000UL;
+unsigned long btnHoldDuration = 2000UL;
 
 const int optionsLength = 5;
 String options[optionsLength] = {"feed", "poo", "play", "doctor", "stats"};
 int currentOption = 0;
+bool atHome = true;
 
 int leftButtonState = 0;
 int rightButtonState = 0;
-int centerButtonState = 0;
+int selectButtonState = 0;
+int backButtonState = 0;
 
-int hunger = 1;
-int poo = 1;
-int happiness = 5;
-int health = 5;
+int hungerLvl = 1;
+int pooLvl = 1;
+int happinessLvl = 5;
+int healthLvl = 5;
 
 void setup() {
   Serial.begin(9600);
   pinMode(leftBtn, INPUT_PULLUP);
   pinMode(rightBtn, INPUT_PULLUP);
-  pinMode(centerBtn, INPUT_PULLUP);
+  pinMode(selectBtn, INPUT_PULLUP);
+  pinMode(backBtn, INPUT_PULLUP);
 }
 
 void loop() {
   leftButtonState = digitalRead(leftBtn);
   rightButtonState = digitalRead(rightBtn);
-  centerButtonState = digitalRead(centerBtn);
+  selectButtonState = digitalRead(selectBtn);
+  backButtonState = digitalRead(backBtn);
 
-  if(leftButtonState == LOW){
+  if(backButtonState == LOW){
+    if(atHome == false){
+      atHome = true;
+      Serial.println("returned home");
+    }
+    Serial.println("pressed back");
+    delay(systemDelay);
+  }
+
+  if(leftButtonState == LOW && atHome == true){
     currentOption--;
     if(currentOption < 0){
       currentOption = 4;  
     }
-    delay(systemDelay);
     Serial.print("current option: ");
     Serial.println(options[currentOption]);
+    delay(systemDelay);
   }
 
-  if(rightButtonState == LOW){
+  if(rightButtonState == LOW && atHome == true){
     currentOption++;
     if(currentOption > optionsLength-1){
       currentOption = 0;
     }
-    delay(systemDelay);
     Serial.print("current option: ");
     Serial.println(options[currentOption]);
+    delay(systemDelay);
   }
 
-  if(centerButtonState == LOW){
-    delay(systemDelay);
+  if(selectButtonState == LOW && atHome == true){
     Serial.print("selected: ");
-     Serial.println(options[currentOption]);
+    Serial.println(options[currentOption]);
+    String selectedOption = options[currentOption];
+    if(selectedOption == "feed"){
+      atHome = false;
+      feed();
+    }
+    if(selectedOption == "poo"){
+      atHome = false;
+      poo();
+    }
+    if(selectedOption == "play"){
+      atHome = false;
+      play();
+    }
+    if(selectedOption == "doctor"){
+      atHome = false;
+      doctor();
+    }
+    if(selectedOption == "stats"){
+      atHome = false;
+      stats();
+    }
+    Serial.print("at home: ");
+    Serial.println(atHome);
+    delay(systemDelay);
   }
   
   unsigned long currentMillis = millis();
 
-  if(currentMillis - previousHungerMillis >= hungerInterval){
-    previousHungerMillis = currentMillis; // Reset the timer
+  if(currentMillis - previoushungerLvlMillis >= hungerLvlInterval){
+    previoushungerLvlMillis = currentMillis; // Reset the timer
 
-  if(hunger < 5){
-      hunger++;
+  if(hungerLvl < 5){
+      hungerLvl++;
     }
-    if(hunger >= 5){
+    if(hungerLvl >= 5){
       // Serial.println("feed this bitch");
     }
-    // Serial.print("hunger: ");
-    // Serial.println(hunger);
+    // Serial.print("hungerLvl: ");
+    // Serial.println(hungerLvl);
   }
 
-  if(currentMillis - previousPooMillis >= pooInterval){
-    previousPooMillis = currentMillis; // Reset the timer
+  if(currentMillis - previouspooLvlMillis >= pooLvlInterval){
+    previouspooLvlMillis = currentMillis; // Reset the timer
 
-    if(poo < 5){
-      poo++;
+    if(pooLvl < 5){
+      pooLvl++;
     }
-    if(poo >= 5){
+    if(pooLvl >= 5){
       // Serial.println("he shid");
-      poo = 1;
+      pooLvl = 1;
     }
-    // Serial.print("poo: ");
-    // Serial.println(poo);
+    // Serial.print("pooLvl: ");
+    // Serial.println(pooLvl);
   }
 
   
+}
+
+void feed() {
+  // select food:
+  // - pear
+  // - cookie
+  // - pizza
+  // - steak
+}
+
+void poo(){
+  // lower poo variable
+  // animation of water washing over screen
+  // remove poo from home screen if present
+  // return home
+}
+
+void play(){
+  // 
+}
+
+void doctor(){
+}
+
+void stats(){
+  // show stats
+  // hunger, poo, happiness, health lvls
+  // set time
+  // reset pet
 }
