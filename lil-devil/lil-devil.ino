42c48a3d334cc80e587285a2822eea5eb6290365
chillbuilds <will@chillbuilds.com>
2024-09-07 20:18:53 -0600

    added left, right, and select buttons

--- lil-devil/lil-devil.ino
+++ lil-devil/lil-devil.ino
@@ -3,6 +3,11 @@
 // - park
 // - 
 
+const int leftBtn = 2;
+const int rightBtn = 3;
+const int centerBtn = 4;
+const int systemDelay = 500;
+
 unsigned long previousHungerMillis = 0;
 unsigned long previousPooMillis = 0;
 // const unsigned long interval = 6 * 3600UL * 1000UL; // 6 hours in milliseconds
@@ -10,31 +15,55 @@ unsigned long hungerInterval = 4000UL;
 unsigned long pooInterval = 2000UL;
 unsigned long hapinessInterval = 3000UL;
 
-int buttonState = 0;
+const int optionsLength = 5;
+String options[optionsLength] = {"feed", "poo", "play", "doctor", "stats"};
+int currentOption = 0;
+
+int leftButtonState = 0;
+int rightButtonState = 0;
+int centerButtonState = 0;
 
 int hunger = 1;
 int poo = 1;
 int happiness = 5;
 int health = 5;
 
-//timing to decriment stats
-
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT_PULLUP);
+  pinMode(leftBtn, INPUT_PULLUP);
+  pinMode(rightBtn, INPUT_PULLUP);
+  pinMode(centerBtn, INPUT_PULLUP);
 }
 
 void loop() {
-  buttonState = digitalRead(2);
+  leftButtonState = digitalRead(leftBtn);
+  rightButtonState = digitalRead(rightBtn);
+  centerButtonState = digitalRead(centerBtn);
 
-  if(buttonState == LOW){
-    Serial.println("fed");
-    hunger--;
-    hunger--;
-    if(hunger < 1){
-      hunger = 1;
+  if(leftButtonState == LOW){
+    currentOption--;
+    if(currentOption < 0){
+      currentOption = 4;  
     }
-    delay(1000);
+    delay(systemDelay);
+    Serial.print("current option: ");
+    Serial.println(options[currentOption]);
+  }
+
+  if(rightButtonState == LOW){
+    currentOption++;
+    if(currentOption > optionsLength-1){
+      currentOption = 0;
+    }
+    delay(systemDelay);
+    Serial.print("current option: ");
+    Serial.println(options[currentOption]);
+  }
+
+  if(centerButtonState == LOW){
+    delay(systemDelay);
+    Serial.print("selected: ");
+     Serial.println(options[currentOption]);
   }
   
   unsigned long currentMillis = millis();
@@ -46,10 +75,10 @@ void loop() {
       hunger++;
     }
     if(hunger >= 5){
-      Serial.println("feed this bitch");
+      // Serial.println("feed this bitch");
     }
-    Serial.print("hunger: ");
-    Serial.println(hunger);
+    // Serial.print("hunger: ");
+    // Serial.println(hunger);
   }
 
   if(currentMillis - previousPooMillis >= pooInterval){
@@ -59,11 +88,11 @@ void loop() {
       poo++;
     }
     if(poo >= 5){
-      Serial.println("he shid");
+      // Serial.println("he shid");
       poo = 1;
     }
-    Serial.print("poo: ");
-    Serial.println(poo);
+    // Serial.print("poo: ");
+    // Serial.println(poo);
   }
 
   
